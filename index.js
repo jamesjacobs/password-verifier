5f565735c862a31630d70fa234ad76af793ff1b6
James Jacobs <jamesjacobs1@mac.com>
2021-05-03 08:18:39 +0100

    Add tests and basic requirements to make tests pass

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 module.exports = function (password) {
     // 1. Password should not be null
+    if (password == null) {
+        throw('A password must be provided');
+    }
     // 2. Password should be larger than 8 chars
+    if (password.length < 9) {
+        throw('Password must be at least 9 characters long');
+    }
     // 3. The password should have one uppercase letter at least
+    if (!(/[A-Z]/.test(password))) {
+        throw("Password must contain at least 1 uppercase letter")
+    };
     // 4. The password should have one lowercase letter at least
+    if (!(/[a-z]/.test(password))) {
+        throw("Password must contain at least 1 lowercase letter")
+    };
     // 5. The password should have one number at least
+    if (!(/[0-9]/.test(password))) {
+        throw("Password must contain at least 1 number")
+    };
+
+    // 🙌 Password meets criteria 🎉
+    return true;
 };
--- spec/tests.spec.js
+++ spec/tests.spec.js
@@ -1,27 +1,51 @@
 const verifyPassword = require('../index');
 
 describe('When no password is provided', () => {
-    it('should throw an error');
+    let password = null;
+
+    it('should throw an error', () => {
+        expect(() => {verifyPassword(password); }).toThrow('A password must be provided');
+    });
 });
 
 describe('When a password is provided', () => {
     describe('if the password has less than 9 chars', () => {
-        it('should throw an error');
+        let password = 'abcdefgh';
+
+        it('should throw an error', () => {
+            expect(() => {verifyPassword(password)}).toThrow('Password must be at least 9 characters long');
+        });
     });
 
     describe('if the password is missing an uppercase letter', () => {
-        it('should throw an error');
+        let password = 'abcdefghi';
+
+        it('should throw an error', () => {
+            expect(() => {verifyPassword(password)}).toThrow('Password must contain at least 1 uppercase letter');
+        });
     });
 
     describe('if the password is missing a lowercase letter', () => {
-        it('should throw an error');
+        let password = 'ABCDEFGHI';
+
+        it('should throw an error', () => {
+            expect(() => {verifyPassword(password)}).toThrow('Password must contain at least 1 lowercase letter');
+        });
     });
 
     describe('if the password is missing at least one number', () => {
-        it('should throw an error');
+        let password = 'AbCDEFGHI';
+
+        it('should throw an error', () => {
+            expect(() => {verifyPassword(password)}).toThrow('Password must contain at least 1 number');
+        });
     });
 
     describe('if the password meets the criteria', () => {
-        it('should return true');
+        let password = 'AbCDEFGH1';
+
+        it('should return true', () => {
+            expect(verifyPassword(password)).toBe(true);
+        });
     });
 });
